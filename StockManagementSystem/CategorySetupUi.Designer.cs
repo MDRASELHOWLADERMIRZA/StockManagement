a1c2cf55d05e245904c6f16098b6e76817959c5a
Md. Rasel Howlader <raselhowlader.me@gmail.com>
2019-06-26 12:33:07 +0600

    Models folder added

--- StockManagementSystem/CategorySetupUi.Designer.cs
+++ StockManagementSystem/CategorySetupUi.Designer.cs
@@ -29,45 +29,28 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.eventLog1 = new System.Diagnostics.EventLog();
-            this.label1 = new System.Windows.Forms.Label();
-            this.CategoryNameTextBox = new System.Windows.Forms.TextBox();
             this.SaveButton = new System.Windows.Forms.Button();
+            this.CategoryNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.ShowCategoryDataGridView = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShowCategoryDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.ShowCategoryDataGridView);
             this.groupBox1.Controls.Add(this.SaveButton);
             this.groupBox1.Controls.Add(this.CategoryNameTextBox);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(35, 39);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(693, 369);
+            this.groupBox1.Size = new System.Drawing.Size(468, 341);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Category Setup";
-            // 
-            // eventLog1
-            // 
-            this.eventLog1.SynchronizingObject = this;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(73, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
-            // 
-            // CategoryNameTextBox
-            // 
-            this.CategoryNameTextBox.Location = new System.Drawing.Point(164, 41);
-            this.CategoryNameTextBox.Name = "CategoryNameTextBox";
-            this.CategoryNameTextBox.Size = new System.Drawing.Size(189, 20);
-            this.CategoryNameTextBox.TabIndex = 1;
             // 
             // SaveButton
             // 
@@ -78,17 +61,46 @@
             this.SaveButton.Text = "Save";
             this.SaveButton.UseVisualStyleBackColor = true;
             // 
-            // CategorySetup
+            // CategoryNameTextBox
+            // 
+            this.CategoryNameTextBox.Location = new System.Drawing.Point(164, 41);
+            this.CategoryNameTextBox.Name = "CategoryNameTextBox";
+            this.CategoryNameTextBox.Size = new System.Drawing.Size(189, 20);
+            this.CategoryNameTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // ShowCategoryDataGridView
+            // 
+            this.ShowCategoryDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ShowCategoryDataGridView.Location = new System.Drawing.Point(32, 137);
+            this.ShowCategoryDataGridView.Name = "ShowCategoryDataGridView";
+            this.ShowCategoryDataGridView.Size = new System.Drawing.Size(399, 172);
+            this.ShowCategoryDataGridView.TabIndex = 3;
+            // 
+            // CategorySetupUi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(764, 436);
+            this.ClientSize = new System.Drawing.Size(556, 436);
             this.Controls.Add(this.groupBox1);
-            this.Name = "CategorySetup";
+            this.Name = "CategorySetupUi";
             this.Text = "CategorySetup";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShowCategoryDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -100,5 +112,6 @@
         private System.Windows.Forms.TextBox CategoryNameTextBox;
         private System.Windows.Forms.Label label1;
         private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.DataGridView ShowCategoryDataGridView;
     }
 }
--- StockManagementSystem/Program.cs
+++ StockManagementSystem/Program.cs
@@ -16,7 +16,8 @@ namespace StockManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Index());
+            //Application.Run(new Index());
+            Application.Run(new CategorySetupUi());
         }
     }
 }
